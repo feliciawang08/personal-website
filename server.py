8705be0015e2598fb70ae6b968bf78594e41a3f9
Felicia <felicia.wang.08@gmail.com>
2020-08-14 22:03:46 -0500

    Sorted files into folders (static folder for script.js and style.css), added favicon, tested username url route.

--- server.py
+++ server.py
@@ -1,17 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 app.debug = True
 
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:favorite_num>')
+def hello_world(username=None, favorite_num=0):
+    print(url_for('static', filename='favicon.ico'))
+    return render_template('index.html', username = username, favorite_num = favorite_num)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/about/blog')
+@app.route('/about')
 def about_blog():
-    return 'Blog'
+    return 'ABOUT BLOG'
