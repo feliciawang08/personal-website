18378e9169209dedf6423698e1253a247908a8cc
Felicia <felicia.wang.08@gmail.com>
2020-08-15 17:57:10 -0500

    Links to each page now work.

--- server.py
+++ server.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 app.debug = True
 
-print(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/<username>/<int:favorite_num>')
-def hello_world(username=None, favorite_num=0):
-    print(url_for('static', filename='favicon.ico'))
-    return render_template('index.html', username = username, favorite_num = favorite_num)
+@app.route('<string:page_name>')
+def page(page_name="/"):
+    try:
+        return render_template(page_name)
+    except:
+        return redirect('/')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
 
-@app.route('/about')
-def about_blog():
-    return 'ABOUT BLOG'
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
